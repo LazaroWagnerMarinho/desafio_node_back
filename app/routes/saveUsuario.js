f13637a1dedb6b2414eb9523fa75b01e946e6380
lazarowagnermarinho@hotmail.com <lazarowagnermarinho@hotmail.com>
2020-12-17 00:59:46 -0300

    exibindo msg de erro na tela, preenchimento obrigatorio

--- app/routes/saveUsuario.js
+++ app/routes/saveUsuario.js
@@ -6,11 +6,31 @@ module.exports = function(aplicacao){
     aplicacao.post('/usuario/save', function(req, res){
         const usuario = req.body;
 
-        const connection = aplicacao.config.dbConnection();
-        const usersModel = new aplicacao.app.models.usersModel(connection);
+        req.assert('nome','Nome e obrigatorio').notEmpty();
+        req.assert('sobreNome','sobreNome e obrigatorio').notEmpty();
+        req.assert('idade','idade e obrigatorio').notEmpty();
+        req.assert('cpf','cpf e obrigatorio').notEmpty();
 
-        usersModel.saveUser(usuario, function(error, resultado){
-            res.redirect('/users');
-        })
+        const erros = req.validationErrors();
+        
+        console.log('formulario erro = ', erros)
+
+        if(erros){
+            console.log('passou por aqui no if de erros!!!! ', erros)
+            const validacao = erros;
+           res.render('users/saveUsuario', {validacao : erros});
+
+        //    res.render("users/users", {usuario : resultado});
+
+            console.log('Validacao  ', validacao)
+            return;
+        }else{
+            const connection = aplicacao.config.dbConnection();
+            const usersModel = new aplicacao.app.models.usersModel(connection);
+    
+            usersModel.saveUser(usuario, function(error, resultado){
+                res.redirect('/users');
+            })
+        }
     })
 }
